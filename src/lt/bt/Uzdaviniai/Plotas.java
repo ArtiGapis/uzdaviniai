0fdadc6313d69fe72ea72a5227aa2c3f05b7d565
Arturas <arturas.danilevicius@gmail.com>
2018-11-14 11:36:09 +0200

    Trikampio plotas

--- src/lt/bt/Uzdaviniai/Plotas.java
+++ src/lt/bt/Uzdaviniai/Plotas.java
@@ -1,10 +1,26 @@
 package lt.bt.Uzdaviniai;
 
+import java.util.Scanner;
+
 public class Plotas {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
 	// write your code here
         System.out.println("hello world");
+    int a,b,c;
+    double P,S;
+    System.out.println("Iveskite krastine a");
+    a = reader.nextInt();
+        System.out.println("Iveskite krastine b");
+    b = reader.nextInt();
+        System.out.println("Iveskite krastine c");
+    c = reader.nextInt();
+    P = a + b + c;
+        P = P/2;
+        S = Math.sqrt(P*(P-a)*(P-b)*(P-c));
+
+        System.out.println("trikapio plotas "+S);
 
 
     }
