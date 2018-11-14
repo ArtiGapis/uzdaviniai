d187c3e387f2703a02a4dcfd4cf0769f6f5cfd0a
Arturas <arturas.danilevicius@gmail.com>
2018-11-14 15:44:26 +0200

    ciklai

---
+++ src/lt/bt/Uzdaviniai/ciklai.java
@@ -0,0 +1,11 @@
+package lt.bt.Uzdaviniai;
+
+public class ciklai {
+    public static void main(String[] args) {
+        System.out.println("Pradzia");
+        for(int i = 0; i < 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
