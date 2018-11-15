ba81cc6bbfd19a25db021a3fed29c5b815754199
Arturas <arturas.danilevicius@gmail.com>
2018-11-15 08:43:28 +0200

    uzduotis2

---
+++ src/lt/bt/Uzdaviniai/Uzduotis2.java
@@ -0,0 +1,18 @@
+package lt.bt.Uzdaviniai;
+
+import java.util.Scanner;
+
+public class Uzduotis2 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Pradzia");
+        int a;
+        System.out.println("Iveskite gimimo metus: ");
+        a = reader.nextInt();
+        a = 2018 - a;
+        if (a>=18){
+            System.out.println("Pilnametis " + a);
+        }
+        else {System.out.println("Nepilnametis " + a);}
+    }
+}
--- src/lt/bt/Uzdaviniai/ciklai.java
+++ src/lt/bt/Uzdaviniai/ciklai.java
@@ -3,7 +3,7 @@ package lt.bt.Uzdaviniai;
 public class ciklai {
     public static void main(String[] args) {
         System.out.println("Pradzia");
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i <= 100; i++) {
             System.out.println(i);
 
         }
