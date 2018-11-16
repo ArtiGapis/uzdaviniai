4f7f11188f5728bca5b9fe4bd13ece582575e22a
Arturas <arturas.danilevicius@gmail.com>
2018-11-16 13:14:01 +0200

    vidurkis

--- src/lt/bt/Uzdaviniai/ciklai.java
+++ src/lt/bt/Uzdaviniai/ciklai.java
@@ -9,7 +9,7 @@ public class ciklai {
             System.out.println(i);
         }
 
-        String filePath = "src\\lt\\bt\\Uzdaviniai\\bandomasis.txt";
+        String filePath = "src\\lt\\bt\\Uzdaviniai\\txt\\bandomasis.txt";
         File file = new File(filePath);
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
@@ -19,7 +19,7 @@ public class ciklai {
         }
         br.close();
 
-        String fileName = "src\\lt\\bt\\Uzdaviniai\\bandomasis1.txt";
+        String fileName = "src\\lt\\bt\\Uzdaviniai\\txt\\bandomasis1.txt";
         String content = "9878";
         FileWriter fw = new FileWriter(fileName);
         BufferedWriter bw = new BufferedWriter(fw);
@@ -28,17 +28,7 @@ public class ciklai {
         bw.close();
         fw.close();
 
-        String filePath1 = "src\\lt\\bt\\Uzdaviniai\\bandomasis1.txt";
-        File file1 = new File(filePath1);
-        BufferedReader br1 = new BufferedReader(new FileReader(file1));
 
-        String st1 =  br1.readLine();
-        System.out.println(st1);
-        //System.out.println(st1.length());
-        //System.out.println(st1.charAt(1));
-        for (int i=0; i < st1.length(); i++) {
-            System.out.println(st1.charAt(i));
-        } System.out.println(st1.length());
     }
 }
 
---
+++ src/lt/bt/Uzdaviniai/vidurkis.java
@@ -0,0 +1,31 @@
+package lt.bt.Uzdaviniai;
+
+import java.io.*;
+
+public class vidurkis {
+    public static void main(String[] args)throws IOException {
+        String filePath1 = "src\\lt\\bt\\Uzdaviniai\\txt\\bandomasis1.txt";
+        File file1 = new File(filePath1);
+        BufferedReader br1 = new BufferedReader(new FileReader(file1));
+        String st1 =  br1.readLine();
+
+        int suma=0;
+        for (int i=0; i < st1.length(); i++)  {
+            suma += Integer.parseInt(String.valueOf(st1.charAt(i)));
+        }
+
+        System.out.println("skaiciu suma: " + suma);
+        double vid=suma/st1.length();
+        System.out.println("skaiciu vidurkis: " + vid);
+
+        String fileName = "src\\lt\\bt\\Uzdaviniai\\txt\\vidurkis.txt";
+        String content = "skaiciu vidurkis: " + vid + "\r\n";
+        FileWriter fw = new FileWriter(fileName);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(String.valueOf(vid + "\r\n"));
+        System.out.println("Done");
+        bw.close();
+        fw.close();
+        br1.close();
+    }
+}
