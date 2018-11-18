93b95df2387e35905a5ad7e6bf38764c5fe511a2
Arturas <arturas.danilevicius@gmail.com>
2018-11-18 23:42:52 +0200

    iki 100

---
+++ src/lt/bt/Uzdaviniai/iki_100.java
@@ -0,0 +1,11 @@
+package lt.bt.Uzdaviniai;
+
+import java.io.IOException;
+
+public class iki_100 {
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
