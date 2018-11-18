7eebe6ee52887ae972ce97370d8fa0820152d3a2
Arturas <arturas.danilevicius@gmail.com>
2018-11-18 23:56:34 +0200

    skaiciu korekcija

--- src/lt/bt/Uzdaviniai/SkaiciausPavadinimas.java
+++ src/lt/bt/Uzdaviniai/SkaiciausPavadinimas.java
@@ -9,48 +9,42 @@ public class SkaiciausPavadinimas {
         System.out.println("Iveskite skaiciu nuo 1 iki 10:");
         int n = reader.nextInt();
 
-        if (n>0&&n<=10) {
-
-            String skaiciai;
-            switch (n) {
-                case 1:
-                    skaiciai = "Vienas";
-                    break;
-                case 2:
-                    skaiciai = "Du";
-                    break;
-                case 3:
-                    skaiciai = "Tris";
-                    break;
-                case 4:
-                    skaiciai = "Keturi";
-                    break;
-                case 5:
-                    skaiciai = "Penki";
-                    break;
-                case 6:
-                    skaiciai = "Sesi";
-                    break;
-                case 7:
-                    skaiciai = "Septyni";
-                    break;
-                case 8:
-                    skaiciai = "Astuoni";
-                    break;
-                case 9:
-                    skaiciai = "Desimt";
-                    break;
-                case 10:
-                    skaiciai = "Nulis";
-                    break;
-                default:
-                    skaiciai = "Invalid month";
-                    break;
-            }
-            System.out.println(skaiciai);
+        String skaiciai;
+        switch (n) {
+            case 1:
+                skaiciai = "Vienas";
+                break;
+            case 2:
+                skaiciai = "Du";
+                break;
+            case 3:
+                skaiciai = "Tris";
+                break;
+            case 4:
+                skaiciai = "Keturi";
+                break;
+            case 5:
+                skaiciai = "Penki";
+                break;
+            case 6:
+                skaiciai = "Sesi";
+                break;
+            case 7:
+                skaiciai = "Septyni";
+                break;
+            case 8:
+                skaiciai = "Astuoni";
+                break;
+            case 9:
+                skaiciai = "Devyni";
+                break;
+            case 10:
+                skaiciai = "Desimt";
+                break;
+            default:
+                skaiciai = "netinkamas skaicius";
+                break;
         }
-        else {
-            System.out.println("netinkama ivestis");
-        }
+        System.out.println(skaiciai);
     }
 }
