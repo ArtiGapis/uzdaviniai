7bdfea04aee5b936c4c51adb9e398e0094c1a611
Arturas <arturas.danilevicius@gmail.com>
2018-11-25 11:28:50 +0200

    LEGO

---
+++ src/lt/bt/Uzdaviniai/papildomi/LEGO.java
@@ -0,0 +1,61 @@
+package lt.bt.Uzdaviniai.papildomi;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LEGO {
+
+    /*
+    Linas nusprendė 6 mėnesius taupyti pinigus LEGO robotui, kuris kainuoja a
+Lt. Kiekvieną mėnesį jis skirtingą kartų kiekį n į taupyklę mesdavo pinigus. Duomenų faile
+Duomenys.txt yra 6 eilutės, kuriose pirmasis skaičius nurodo, kiek kartų n tą mėnesį Linas į
+taupyklę įmetė pinigų, o kiti skaičiai toje eilutėje nurodo, kiek pinigų m (litais) Linas įmetė.
+Duomenų failo paskutiniojoje (septintoje) eilutėje yra nurodyta norimo LEGO roboto kaina a
+(litais). Parašykite programą, kuri suskaičiuotų, kiek Linas per šešis mėnesius sutaupė pinigų ir
+ar jų užteks nusipirkti norimą LEGO robotą, o rezultatus įrašytų į rezultatų failą Rezultatai.txt.
+     */
+
+    public static void main(String[] args) throws IOException {
+        Scanner reader = new Scanner(System.in);
+        int a = 0; //roboto kaina (septinta eilute)
+        int n = 0;//imesta i taupykle (faile pirmas)
+        int m = 0; //kiek imete (5 eilutes)
+
+        duomenysWraper();
+        bandymas2dimenciju();
+    }
+
+    private static void duomenysWraper() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("startas");
+
+        int sum = 0;
+        int[] array = new int[7];
+
+        for (int i = 0; i <= 6; i++) {
+            array[i]= i;
+            System.out.println(array[i]);
+
+        }
+    }
+
+    private static void bandymas2dimenciju() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("startas");
+
+        int numbers[][];
+        numbers = new int[5][5];
+
+        for (int r=0; r < numbers.length; r++) {
+            for (int c=0; c < numbers[r].length; c++) {
+                System.out.print(" " + numbers[r][c]);
+                numbers[0][2]=2;
+                numbers[0][3]=3;
+            }
+            System.out.println("");
+        }
+        numbers[0][0]=numbers[0][1]+numbers[0][2]+numbers[0][3]+numbers[0][4];
+        System.out.println(numbers[0][0]);
+    }
+
+}
