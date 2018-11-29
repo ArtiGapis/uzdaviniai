dc99ed4cdcc499fe7a9eb53205d7073ceffcb03b
Arturas <arturas.danilevicius@gmail.com>
2018-11-29 12:45:47 +0200

    Final Kontrolinis

--- src/lt/bt/Uzdaviniai/Kontrolinis/VariantasB.java
+++ src/lt/bt/Uzdaviniai/Kontrolinis/VariantasB.java
@@ -10,18 +10,13 @@ public class VariantasB {
     public static void main(String[] args) throws IOException {
 
         int sum = 0;
-        int maximum = 0;
-        int minimum = 0;
         int min=0;
         int max=0;
 
-        read( sum, min, max, minimum, maximum);
-
-
-
+        read( sum, min, max);
     }
 
-    private static void read (int sum,int min,int max,int minimum,int maximum){
+    private static void read (int sum,int min,int max){
         try {
             String filePath = "src\\lt\\bt\\Uzdaviniai\\txt\\DuomenysB.txt";
             File file = new File(filePath);
@@ -42,18 +37,17 @@ public class VariantasB {
                 for (int i = 0; i < stopsCount; i++) {
                     if (array[i] > max) {
                         max = array[i];
-                        maximum = max;
                     } else if (array[i] < min) {
                         min = array[i];
-                        minimum = min;
                     }
                 }
                 printToConsole(sum, min, max);
-                writeToFiles(sum,minimum,maximum);
+                writeToFiles(sum,min,max);
 
             }scanner.close();
         }catch (IOException e){ System.err.println("Nera failo");}
     }
+
     private static void printToConsole(int sum,int min,int max) throws IOException {
         System.out.println("REZULTATU FAILAS"); // ats pirmas
         System.out.println("____________________");
@@ -62,12 +56,12 @@ public class VariantasB {
         System.out.println("Didziausa: " + max); //Ats trecias
     }
 
-    private static void writeToFiles(int sum,int minimum,int maximum) throws IOException {
+    private static void writeToFiles(int sum,int min,int max) throws IOException {
         String fileName = "src\\lt\\bt\\Uzdaviniai\\txt\\Rezultatai.txt";
         String r = "Rezultatų failas \r\n";
-        String suma = "bendras atstumas: " + sum +" m."+"\r\n";
-        String minimumas = "Maziausias: " + minimum+" m."+"\r\n";
-        String maximumas = "Didziausias: " + maximum+" m."+"\r\n";
+        String suma = "Maršruto atstumas: " + sum +" m."+"\r\n";
+        String minimumas = "Trumpiausias atstumas: " + min+" m."+"\r\n";
+        String maximumas = "Ilgiausias atstumas: " + max+" m."+"\r\n";
         FileWriter fw = new FileWriter(fileName);
         BufferedWriter bw = new BufferedWriter(fw);
         bw.write(r);
