4b0b67d08b8f8b6242a3fe3cdabb7f9c00a24e9e
Arturas <arturas.danilevicius@gmail.com>
2018-11-29 12:35:34 +0200

    Kontrolinis 1

--- src/lt/bt/Uzdaviniai/Kontrolinis/VariantasB.java
+++ src/lt/bt/Uzdaviniai/Kontrolinis/VariantasB.java
@@ -8,31 +8,37 @@ import java.util.Scanner;
 
 public class VariantasB {
     public static void main(String[] args) throws IOException {
+
+        int sum = 0;
+        int maximum = 0;
+        int minimum = 0;
+        int min=0;
+        int max=0;
+
+        read( sum, min, max, minimum, maximum);
+
+
+
+    }
+
+    private static void read (int sum,int min,int max,int minimum,int maximum){
         try {
             String filePath = "src\\lt\\bt\\Uzdaviniai\\txt\\DuomenysB.txt";
             File file = new File(filePath);
             Scanner scanner = new Scanner(file);
-            int sum = 0;
-            int maximum = 0;
-            int minimum = 0;
-            int min=0;
-            int max=0;
+
             int stopsCount = scanner.nextInt(); //stoteles
             int[] array = new int[stopsCount];
 
-
-
-
             while (scanner.hasNextLine()) {
-
                 for (int i = 0; i < stopsCount; i++) {
                     array[i] = scanner.nextInt();
                     sum = sum + array[i];
-
-
                 }
+
                 max = array[0];
                 min = array[0];
+
                 for (int i = 0; i < stopsCount; i++) {
                     if (array[i] > max) {
                         max = array[i];
@@ -42,22 +48,13 @@ public class VariantasB {
                         minimum = min;
                     }
                 }
-
+                printToConsole(sum, min, max);
+                writeToFiles(sum,minimum,maximum);
 
             }scanner.close();
-
-            printConsole(sum, min, max);
-            print(sum,minimum,maximum);
-
-        }catch (IOException e){
-            System.err.println("Nera failo");}
+        }catch (IOException e){ System.err.println("Nera failo");}
     }
-
-    private static void reader() throws IOException {
-
-    }
-
-    private static void printConsole(int sum,int min,int max) throws IOException {
+    private static void printToConsole(int sum,int min,int max) throws IOException {
         System.out.println("REZULTATU FAILAS"); // ats pirmas
         System.out.println("____________________");
         System.out.println("Bendras atstumas:" + sum); // ats pirmas
@@ -65,13 +62,12 @@ public class VariantasB {
         System.out.println("Didziausa: " + max); //Ats trecias
     }
 
-
-    private static void print(int sum,int minimum,int maximum) throws IOException {
+    private static void writeToFiles(int sum,int minimum,int maximum) throws IOException {
         String fileName = "src\\lt\\bt\\Uzdaviniai\\txt\\Rezultatai.txt";
-        String r = "Rezultatų failas \n";
-        String suma = "bendras atstumas: " + sum +"\n";
-        String minimumas = "Maziausias: " + minimum+"\n";
-        String maximumas = "Didziausias: " + maximum+"\n";
+        String r = "Rezultatų failas \r\n";
+        String suma = "bendras atstumas: " + sum +" m."+"\r\n";
+        String minimumas = "Maziausias: " + minimum+" m."+"\r\n";
+        String maximumas = "Didziausias: " + maximum+" m."+"\r\n";
         FileWriter fw = new FileWriter(fileName);
         BufferedWriter bw = new BufferedWriter(fw);
         bw.write(r);
