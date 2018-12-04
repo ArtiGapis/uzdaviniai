40111048440403e4c25713382c6ea7ac17046b5e
Arturas <arturas.danilevicius@gmail.com>
2018-12-04 10:56:06 +0200

    isskirstymas

--- src/lt/bt/Uzdaviniai/papildomi/Padangu_uzd/Padangos.java
+++ src/lt/bt/Uzdaviniai/papildomi/Padangu_uzd/Padangos.java
@@ -4,46 +4,78 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class Padangos {
     public static void main(String[] args) throws IOException {
 
-        read();
+        calc();
     }
 
-    private static void read() throws IOException {
+    private static String read() {
         String filePath = "src\\lt\\bt\\Uzdaviniai\\txt\\padanga.txt";
         File file = new File(filePath);
-        Scanner scanner = new Scanner(file);
+        //Scanner scanner = new Scanner(file);
+        return filePath;
+    }
 
+    private static int plot() throws IOException {
+        File file = new File(read());
+        Scanner scanner = new Scanner(file);
         int plotis = scanner.nextInt();
         //System.out.println("plotis - " + plotis);
+        return plotis;
+    }
 
+    private static int auks() throws IOException {
+        File file = new File(read());
+        Scanner scanner = new Scanner(file);
+        scanner.nextInt();
         int aukstis = scanner.nextInt();
         //System.out.println("aukstis - " + aukstis);
+        return aukstis;
+    }
 
+    private static String index() throws IOException {
+        File file = new File(read());
+        Scanner scanner = new Scanner(file);
+        scanner.nextInt();
+        scanner.nextInt();
         String greit = scanner.nextLine();
-        //System.out.println("greitis - " + greit);
-
         greit = greit.trim();
+        //System.out.println("greitis - " + greit);
+        return greit;
+    }
 
+    private static int arrayNumber() throws IOException {
+        File file = new File(read());
+        Scanner scanner = new Scanner(file);
+        scanner.nextInt();
+        scanner.nextInt();
+        scanner.nextLine();
         int arrayNum = scanner.nextInt(); //masyvo ilgis
         //System.out.println("ilgis - " + arrayNum);
+        return arrayNum;
+    }
 
-
+    private static void calc() throws IOException {
+        int arrayNum=arrayNumber();
         Ratas[] padang = new Ratas[arrayNum];
         double bendraKaina=0;
         int kiekis=0;
+        File file = new File(read());
+        Scanner scanner = new Scanner(file);
+        scanner.nextInt();
+        scanner.nextInt();
+        scanner.nextLine();
+        scanner.nextInt();
+
         for (int i = 0; i < arrayNum; i++) {
             Ratas padangaSandeliy = new Ratas();
-
             padangaSandeliy.setPlotis(scanner.nextInt());
             padangaSandeliy.setAukstis(scanner.nextInt());
             padangaSandeliy.setGreit(scanner.next());
             padangaSandeliy.setKaina(scanner.nextDouble());
             padang[i] = padangaSandeliy;
-
-            if (padangaSandeliy.getPlotis() == plotis && padangaSandeliy.getAukstis() == aukstis&&padangaSandeliy.getGreit().equals(greit)) {
+            if (padangaSandeliy.getPlotis() ==plot() && padangaSandeliy.getAukstis() == auks()&&padangaSandeliy.getGreit().equals(index())) {
                 //System.out.println(padangaSandeliy);
                 bendraKaina=padangaSandeliy.getKaina()+bendraKaina;
                 kiekis++;
@@ -51,10 +83,5 @@ public class Padangos {
         }
         System.out.println("Tinkamų padangų skaičius: "+kiekis);
         System.out.println("Jos kainuotų: "+bendraKaina);
-    }
-
-    public void print() {
-        //System.out.println(bendraKaina);
-        //System.out.println(kiekis);
     }
 }
